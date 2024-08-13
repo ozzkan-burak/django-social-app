987f309ee21126fd0ad4ed0c8af0438e6a7d63b1
ozzkan-burak <brk.ozzkan@gmail.com>
2024-08-13 23:26:08 +0300

    episode1

--- socialmedia/Appsocial/models.py
+++ socialmedia/Appsocial/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   image = models.ImageField(upload_to="Post-Image")
   description = models.TesxtField()
+  likes = models.ManyToManyField(User)
   date = models.DateTimeField(auto_now_add=True)
   
 class Commment(models.Model):
@@ -12,3 +13,16 @@ class Commment(models.Model):
   post = models.ForeignKey(Post, on_delete=models.CASCADE)
   comment = models.TextField()
   date = models.DateTimeField(auto_now_add=True)
+  
+class Profile(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  image = models.ImageField(upload_to="Profile-Image")
+  bio = models.TextField()
+
+class Follow(models.Model):
+  profile = models.ForeignKey(Profile, releated_name="profile" on_delete=models.CASCADE)
+  followed = models.ForeignKey(Profile, releated_name="followed" on_delete=models.CASCADE)
+  
+class Follower(models.Model):
+  profile = models.ForeignKey(Profile, releated_name="profile" on_delete=models.CASCADE)
+  following = models.ForeignKey(Profile, releated_name="following" on_delete=models.CASCADE)
