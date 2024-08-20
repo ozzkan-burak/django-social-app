792bfa526f75112a6b6b3ca8a15bc3ccf7207536
ozzkan-burak <brk.ozzkan@gmail.com>
2024-08-20 21:37:58 +0300

    episode2

--- socialmedia/Appsocial/admin.py
+++ socialmedia/Appsocial/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Post)
+admin.site.register(Commment)
+admin.site.register(Profile)
+admin.site.register(Follow)
+admin.site.register(Follower)
 
 # Register your models here.
