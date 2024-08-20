b60da9309021788f7e1cc0639c9d353342fe7f79
ozzkan-burak <brk.ozzkan@gmail.com>
2024-08-20 23:28:04 +0300

    episode4

--- socialmedia/socialmedia/urls.py
+++ socialmedia/socialmedia/urls.py
@@ -7,5 +7,9 @@ from Appsocial.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("login/", Login, name="login"),
+    path("register/", Register, name="register"),
+    path("profile/", Profile, name="profile"),
+    
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
